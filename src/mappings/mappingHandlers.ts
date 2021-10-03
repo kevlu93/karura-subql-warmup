154b34c8a4875929ccaf114a873c9115497e7649
Kevin Lu <kevlu93@gmail.com>
2021-10-03 12:51:05 +0200

    use an array instead of entities

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,13 +1,7 @@
 import { SubstrateEvent } from "@subql/types";
-import { BalanceTransferSummary } from "../types/models/BalanceTransferSummary";
-import {Transfer} from "../types/models/Transfer";
+import { BalanceTransferSummary } from "../types";
+//import {Transfer} from "../types/models/Transfer";
 import { Balance } from "@polkadot/types/interfaces";
-
-const median = (arr) => {
-  const mid = Math.floor(arr.length / 2),
-    nums = [...arr].sort((a, b) => a - b);
-  return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
-};
 
 function createBalanceTransferSummary(accountId: string): BalanceTransferSummary {
   const entity = new BalanceTransferSummary(accountId);
@@ -15,6 +9,12 @@ function createBalanceTransferSummary(accountId: string): BalanceTransferSummary
   entity.countTransferIn = BigInt(0);
   entity.totalTransferOut = BigInt(0);
   entity.countTransferOut = BigInt(0);
+  entity.avgTransferIn = BigInt(0);
+  entity.avgTransferOut = BigInt(0);
+  entity.medianTransferIn = BigInt(0);
+  //entity.medianTransferOut = BigInt(0);
+  //entity.transfersIn = [];
+  //entity.transfersOut = [];
   return entity;
 }
 
@@ -33,30 +33,26 @@ export async function handleBalanceTransferSummaryEvent(event: SubstrateEvent): 
     receiveEntity = createBalanceTransferSummary(to.toString());
   }
 
-  let giveEntity = await BalanceTransferSummary.get(from.toString());
-  if (giveEntity == undefined) {
-    giveEntity = createBalanceTransferSummary(from.toString());
-  }
-
-  const transferInfo = new Transfer(
-    `${event.block.block.header.number.toNumber()}-${event.idx}`,
-  )
-
-  transferInfo.fromId = from.toString();
-  transferInfo.toId = to.toString();
-  transferInfo.amount = (amount as Balance).toBigInt();
+  
 
   receiveEntity.totalTransferIn += BigInt(amount.toString());
   receiveEntity.countTransferIn += BigInt(1);
   receiveEntity.avgTransferIn = receiveEntity.totalTransferIn / receiveEntity.countTransferIn;
+  //receiveEntity.transfersIn.push(BigInt(amount.toString()));
+  //receiveEntity.medianTransferIn += BigInt(0);
   
- // receiveEntity.medianTransferIn = median(receiveEntity.transfersIn)
   await receiveEntity.save();
 
+  let giveEntity = await BalanceTransferSummary.get(from.toString());
+  if (giveEntity == undefined) {
+    giveEntity = createBalanceTransferSummary(from.toString());
+  }
   
   giveEntity.totalTransferOut += BigInt(amount.toString());
   giveEntity.countTransferOut += BigInt(1);
   giveEntity.avgTransferOut = giveEntity.totalTransferOut / giveEntity.countTransferOut;
-  
+  //giveEntity.transfersOut.push(BigInt(amount.toString()));
+  //giveEntity.medianTransferOut= median(giveEntity.transfersOut);
+
   await giveEntity.save();
 }
